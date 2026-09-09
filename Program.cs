c45c6a6d03cb9d66f5f75051e54c36f620da15de
Babu <2380223@cognizant.com>
2026-09-09 17:09:06 +0530

    Added Entity Framework Core and SQLite setup

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
+ var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
-options.UseInMemoryDatabase("UrlShortnerDb"));
+options.UseSqlite(connectionString));
 builder.Services.AddScoped<IUrlService, UrlService>();
 builder.Services.AddControllers();
 
